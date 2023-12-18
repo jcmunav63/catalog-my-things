427a8be6b6b2a0c01b0c687a55f2209d75ac1920
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-12-18 15:54:35 -0600

    Feat: add item class with properties and methods.

---
+++ item.rb
@@ -0,0 +1,17 @@
+class Item
+  def initialize(genre, author, source, label, publish_date)
+    @id = Random.rand(1..1000)
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = publish_date
+    @archived = false
+  end
+
+  def can_be_archived?()
+  end
+
+  def move_to_archived()
+  end
+end
