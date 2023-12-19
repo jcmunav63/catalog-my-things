dcef5df25b286987a33e3c022bb3f509b416b869
jcmunav63 <jcmunav63@gmail.com>
2023-12-19 14:17:33 -0600

    Feat: Create new musicalbum method and adapt menu.

--- music_album.rb
+++ music_album.rb
@@ -9,4 +9,22 @@ class MusicAlbum < Item
   def can_be_archived?
     super && @on_spotify
   end
+
+  # def create_musicalbum
+  #   print 'Genre: '
+  #   genre = gets.chomp
+  #   print 'Author: '
+  #   author = gets.chomp
+  #   print 'Source: '
+  #   source = gets.chomp
+  #   print 'Label: '
+  #   label = gets.chomp
+  #   print 'Publish date: '
+  #   publish_date = gets.chomp
+  #   print 'On Spotify (true / false): '
+  #   on_spotify = gets.chomp
+  #   @musicalbums << MusicAlbum.new(genre, author, source, label, publish_date, on_spotify)
+  #   puts 'Music Album created successfully!'
+  #   puts
+  # end
 end
