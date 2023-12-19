4d27082e06e101cd2096d9bb8489758843e66eff
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-12-19 13:14:38 -0600

    Refactor: catalog.rb to only use needed methods.

--- catalog.rb
+++ catalog.rb
@@ -4,18 +4,6 @@ class Catalog
     @labels = []
   end
 
-  def create_item
-    puts 'Create item'
-  end
-
-  def archive_item
-    puts 'Archive item'
-  end
-
-  def list_all_items
-    puts 'List all items'
-  end
-
   def list_all_book
     puts 'List all book'
   end
