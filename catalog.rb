e68201ebc2e92a6df90ec7735a101f342fac6726
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-12-19 12:32:21 -0600

    Feat: add catalog class to apply separation of concerns.

---
+++ catalog.rb
@@ -0,0 +1,30 @@
+class Catalog
+  def initialize
+    @items = []
+    @labels = []
+  end
+
+  def create_item
+    puts 'Create item'
+  end
+
+  def archive_item
+    puts 'Archive item'
+  end
+
+  def list_all_items
+    puts 'List all items'
+  end
+
+  def list_all_book
+    puts 'List all book'
+  end
+
+  def list_all_labels
+    puts 'List all labels'
+  end
+
+  def add_a_book
+    puts 'Add a book'
+  end
+end
