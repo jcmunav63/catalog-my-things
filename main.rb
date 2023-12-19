9b34872496c66c681ebdabe4f9a5ae25942d7e12
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-12-19 15:20:35 -0600

    Feat: update main menu.

--- main.rb
+++ main.rb
@@ -24,9 +24,18 @@ class Main
 
   def display_menu
     puts '1) List all books'
-    puts '2) List all labels'
-    puts '3) Add a book'
-    puts '4) Exit the app'
+    puts '2) List all music albums'
+    puts '3) List all movies'
+    puts '4) List of games'
+    puts '5) List all genres'
+    puts '6) List all labels'
+    puts '7) List all authors'
+    puts '8) List all sources'
+    puts '9) Add a book'
+    puts '10) Add a music album'
+    puts '11) Add a movie'
+    puts '12) Add a game'
+    puts '13) Exit the app'
   end
 
   def gets_option
